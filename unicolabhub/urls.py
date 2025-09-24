83b0f8c8bad5017141101167f9df5c5805a03707
AshF000 <ashfiq_islam@yahoo.com>
2025-09-25 00:03:23 +0600

    first commit

---
+++ unicolabhub/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.shortcuts import redirect
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("home/", lambda request: redirect("home")),
+]
