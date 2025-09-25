a76e45c14d200dc0f66749948ff96be93aabc4de
AshF000 <ashfiq_islam@yahoo.com>
2025-09-25 17:54:09 +0600

    login fix, register add

--- user/urls.py
+++ user/urls.py
@@ -5,8 +5,11 @@ from django.urls import path, include
 
 from . import views
 
+app_name = "user"
 
 urlpatterns = [
+    path('register/', views.register_user, name='register_user'),
+    path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout_user'),
     path('user_profile/<str:username>/', views.user_profile, name='user_profile'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,10 +1,49 @@
 from django.contrib import messages
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 
 # Create your views here.
+def register_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Optional: Check if username already exists
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already taken.")
+            return redirect('register_user')
+
+        # Create the user
+        user = User.objects.create_user(username=username, password=password)
+        user.is_staff = False  # Cannot access admin
+        user.is_superuser = False  # Definitely not admin
+        user.save()
+
+        return redirect('user:login_user')
+
+        # # Log the user in
+        # user = authenticate(username=username, password=password)
+        # if user is not None:
+        #     login(request, user)
+        #     return redirect('home')  # Replace 'home' with your landing page name
+
+    return render(request, 'user/register_form.html')  # Show registration form
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid credentials")
+    else:
+        return render(request, "user/login_form.html")
+
 
 def logout_user(request):
     logout(request)
