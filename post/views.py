0b904f114809b60fd1a0c56c7acc36cc6168f7e6
AshF000 <ashfiq_islam@yahoo.com>
2025-10-17 01:27:17 +0600

    post-event:[manual form, views, create.html, home.html]

--- post/views.py
+++ post/views.py
@@ -1,23 +1,46 @@
 from django.shortcuts import render, redirect
-from .forms import ProjectForm,EventForm,ThesisForm
-from .models import Project,Event,Thesis
+from django.utils.dateparse import parse_datetime
+
+from .forms import ProjectForm, EventForm, ThesisForm
+from .models import Project, Event, Thesis
 from collaboration.models import Collaborator
+
 
 def create_event(request):
     if request.method == "POST":
-        form = EventForm(request.POST)
-        if form.is_valid():
-            event = form.save(commit=False)
-            collaborator = Collaborator(user=request.user, post=None)
-            collaborator.save()
-            event.organizer = collaborator
-            event.save()
-            collaborator.post = event
-            collaborator.save()
-            return redirect("home")
-    else:
-        form = EventForm()
-    return render(request,'post/create_event.html',{"form":form})
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        location = request.POST.get('location')
+        status = request.POST.get('status')
+
+        cover_pic = request.FILES.get('cover_pic')
+        schedule = request.FILES.get('schedule')
+
+        start_time = parse_datetime(request.POST.get('start_time'))
+        end_time = parse_datetime(request.POST.get('end_time'))
+
+        collaborator = Collaborator(user=request.user, post=None)
+        collaborator.save()
+
+        event = Event.objects.create(
+            organizer=collaborator,
+            title=title,
+            description=description,
+            location=location,
+            status=status,
+            start_time=start_time,
+            end_time=end_time,
+            cover_pic=cover_pic,
+            schedule=schedule,
+        )
+
+        collaborator.post = event
+        collaborator.save()
+
+        return redirect('home')
+
+    return render(request, 'post/create_event.html')
+
 
 def create_thesis(request):
     if request.method == "POST":
@@ -33,7 +56,8 @@ def create_thesis(request):
             return redirect("home")
     else:
         form = ThesisForm()
-    return render(request,'post/create_thesis.html',{"form":form})
+    return render(request, 'post/create_thesis.html', {"form": form})
+
 
 def create_project(request):
     if request.method == "POST":
@@ -51,47 +75,53 @@ def create_project(request):
     else:
         form = ProjectForm()
 
-    return render(request,'post/create_project.html',{"form":form})
+    return render(request, 'post/create_project.html', {"form": form})
 
-def view_project(request,pk):
+
+def view_project(request, pk):
     project = Project.objects.get(pk=pk)
-    return render(request,'post/view_project.html',{"project":project})
+    return render(request, 'post/view_project.html', {"project": project})
 
-def view_thesis(request,pk):
+
+def view_thesis(request, pk):
     thesis = Thesis.objects.get(pk=pk)
-    return render(request,'post/view_thesis.html',{"thesis":thesis})
+    return render(request, 'post/view_thesis.html', {"thesis": thesis})
 
-def view_event(request,pk):
+
+def view_event(request, pk):
     event = Event.objects.get(pk=pk)
-    return render(request,'post/view_event.html',{"event":event})
+    return render(request, 'post/view_event.html', {"event": event})
 
-def edit_project(request,pk):
+
+def edit_project(request, pk):
     project = Project.objects.get(pk=pk)
     if request.method == "POST":
-        form = ProjectForm(request.POST,instance=project)
+        form = ProjectForm(request.POST, instance=project)
         if form.is_valid():
             project = form.save(commit=False)
             return redirect("home")
     else:
         form = ProjectForm(instance=project)
-    return render(request,'post/edit_project.html',{"form":form})
+    return render(request, 'post/edit_project.html', {"form": form})
 
-def edit_thesis(request,pk):
+
+def edit_thesis(request, pk):
     thesis = Thesis.objects.get(pk=pk)
     if request.method == "POST":
-        form = ThesisForm(request.POST,instance=thesis)
+        form = ThesisForm(request.POST, instance=thesis)
         if form.is_valid():
             thesis = form.save(commit=False)
             return redirect("home")
     else:
         form = ThesisForm(instance=thesis)
 
-    return render(request,'post/edit_thesis.html',{"form":form})
+    return render(request, 'post/edit_thesis.html', {"form": form})
 
-def edit_event(request,pk):
+
+def edit_event(request, pk):
     event = Event.objects.get(pk=pk)
     if request.method == "POST":
-        form = EventForm(request.POST,instance=event)
+        form = EventForm(request.POST, instance=event)
         if form.is_valid():
             event = form.save(commit=False)
             return redirect("home")
@@ -99,25 +129,28 @@ def edit_event(request,pk):
     else:
         form = EventForm(instance=event)
 
-    return render(request,'post/edit_event.html',{"form":form})
+    return render(request, 'post/edit_event.html', {"form": form})
 
-def delete_project(request,pk):
+
+def delete_project(request, pk):
     project = Project.objects.get(pk=pk)
     if request.method == "POST":
         project.delete()
         return redirect("home")
-    return render(request,'post/delete_project.html',{"project":project})
+    return render(request, 'post/delete_project.html', {"project": project})
 
-def delete_thesis(request,pk):
+
+def delete_thesis(request, pk):
     thesis = Thesis.objects.get(pk=pk)
     if request.method == "POST":
         thesis.delete()
         return redirect("home")
-    return render(request,'post/delete_thesis.html',{"thesis":thesis})
+    return render(request, 'post/delete_thesis.html', {"thesis": thesis})
 
-def delete_event(request,pk):
+
+def delete_event(request, pk):
     event = Event.objects.get(pk=pk)
     if request.method == "POST":
         event.delete()
         return redirect("home")
-    return render(request,'post/delete_event.html',{"event":event})
+    return render(request, 'post/delete_event.html', {"event": event})
